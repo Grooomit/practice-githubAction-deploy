94066cc7127afb277f7b170d10d1a885974effaf
Grooomit <tmd5972@gmail.com>
2022-10-07 17:39:17 +0900

    practice githubActions

---
+++ src/main/java/com/codestates/practicegithubActiondeployment/PracticeGithubActionDeploymentApplication.java
@@ -0,0 +1,13 @@
+package com.codestates.practicegithubActiondeployment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeGithubActionDeploymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeGithubActionDeploymentApplication.class, args);
+	}
+
+}
